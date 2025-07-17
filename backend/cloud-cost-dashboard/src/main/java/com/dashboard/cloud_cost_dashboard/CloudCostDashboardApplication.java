02599f604299f09bd8f67f0dbf5753c9a2fe7cfd
Katavina Nguyen <knguyen1.ga@gmail.com>
2025-07-17 17:55:16 -0400

    initialize springboot app

---
+++ backend/cloud-cost-dashboard/src/main/java/com/dashboard/cloud_cost_dashboard/CloudCostDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.cloud_cost_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCostDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCostDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/cloud-cost-dashboard/src/test/java/com/dashboard/cloud_cost_dashboard/CloudCostDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.cloud_cost_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCostDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
